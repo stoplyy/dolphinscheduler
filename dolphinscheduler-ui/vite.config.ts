72a81f9d94360efd3663a49d3fe899f6976c6b8e
liuyangyang <liuyangyang@tuhu.cn>
2024-05-23 15:22:48 +0800

    change ui config

--- dolphinscheduler-ui/vite.config.ts
+++ dolphinscheduler-ui/vite.config.ts
@@ -1,53 +1,41 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import viteCompression from 'vite-plugin-compression'
 import path from 'path'
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/dolphinscheduler/ui/' : '/',
-  plugins: [
-    vue(),
-    vueJsx(),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
-      deleteOriginFile: false
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-      // resolve vue-i18n warning: You are running the esm-bundler build of vue-i18n.
-    }
-  },
-  server: {
-    port: 8380,
-    proxy: {
-      '/dolphinscheduler': {
-        target: loadEnv('development', './').VITE_APP_DEV_WEB_URL,
-        changeOrigin: true
+// `mode` 是从命令行参数 `--mode` 传入的值
+export default defineConfig(({ mode }) => {
+  // 使用 `mode` 从项目根目录加载相应的环境变量
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  return {
+    base: env.VITE_USER_NODE_ENV === '' ? '/dolphinscheduler/ui/' : '/',
+    plugins: [
+      vue(),
+      vueJsx(),
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz',
+        deleteOriginFile: false
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    server: {
+      proxy: {
+        '/dolphinscheduler': {
+          // 使用加载的环境变量
+          target: env.VITE_APP_DEV_WEB_URL,
+          changeOrigin: true
+        }
       }
     }
-  }
-})
+  };
+});
