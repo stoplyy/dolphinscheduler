06a07fbf442c8e4b8e5f0a79592c168aad14073d
liuyangyang <liuyangyang@tuhu.cn>
2024-09-09 16:36:01 +0800

    fix jatest env error

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/platform/common/EnvironmentUtil.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/platform/common/EnvironmentUtil.java
@@ -1,6 +1,7 @@
 package org.apache.dolphinscheduler.api.platform.common;
 
 import com.tuhu.commons.environment.BasicEnvironment;
+import com.tuhu.commons.environment.enums.EnvEnum;
 
 public class EnvironmentUtil {
     public static String getAppId() {
@@ -9,23 +10,30 @@ public class EnvironmentUtil {
         return appId;
     }
 
-    public static String getEnv() {
-        String environment = BasicEnvironment.getInstance().getEnvironment();
-        return environment;
-    }
-
     public static String getEnvironment() {
         String env = BasicEnvironment.getInstance().getEnvironment();
         if ("prod".equals(env) || "production".equals(env) || "prd".equals(env)) {
             return "prod";
         }
-        if ("work".equals(env)) {
-            return "work";
-        }
         if ("tuhutest".equals(env)) {
             return "test";
         }
+        EnvEnum envEnum = getEnvEnum();
+        if(envEnum != null) {
+            return envEnum.getCode();
+        }
         return "work";
+    }
+
+    public static EnvEnum getEnvEnum() {
+        String env = BasicEnvironment.getInstance().getEnvironment();
+        // env转换为EnvEnum
+        for (EnvEnum envEnum : EnvEnum.values()) {
+            if (envEnum.getCode().equalsIgnoreCase(env)) {
+                return envEnum;
+            }
+        }
+        return null;
     }
 
     public static boolean isProd() {
